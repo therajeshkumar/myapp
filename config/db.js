13fdbf9e8c9357b4e431afa099a71db2467a5f1c
therajeshkumar <49233541+therajeshkumar@users.noreply.github.com>
2020-03-02 09:52:06 +0530

    db change
    dd

--- config/db.js
+++ config/db.js
@@ -1,5 +1,3 @@
 module.exports = {
- // db: 'mongodb://localhost:27017/mean',
-  db: 'mongodb://maurya:maurya123@ds063186.mlab.com:63186/mean',
- // db: 'mongodb://maurya:maurya123@ds135852.mlab.com:35852/maurya',
+ db: 'mongodb://maurya:maurya123@ds135852.mlab.com:35852/maurya'
  };
